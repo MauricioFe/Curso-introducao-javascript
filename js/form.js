59013c9e9e76049ce990681ad36a73023c5d9e36
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-07-02 23:20:44 -0300

    implementação de uma validação de formulário

--- js/form.js
+++ js/form.js
@@ -6,8 +6,16 @@ boataoAdicionar.addEventListener("click", (event) => {
     var form = document.querySelector("#form-adiciona");
     if (form.nome.value != "" && form.peso.value != "" && form.altura.value != "" && form.gordura.value != "") {
         var paciente = getValoreDoForm(form);
-        var tabela = document.querySelector("#tabela-pacientes")
         var pacienteTr = montarTr(paciente)
+        var erros = validaPaciente(paciente);
+        console.log(erros)
+        if (erros.length > 0) {
+            exibeMensagensDeErro(erros);
+            return;
+        }
+
+        var tabela = document.querySelector("#tabela-pacientes")
+
         tabela.appendChild(pacienteTr);
         form.reset();
     } else {
@@ -26,13 +34,13 @@ function getValoreDoForm(form) {
     return paciente;
 }
 function montarTr(paciente) {
-  
+
     var pacienteTr = document.createElement("tr");
     pacienteTr.classList.add("paciente");
-    pacienteTr.appendChild( montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
     pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
     pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
-    pacienteTr.appendChild( montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
     pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
     return pacienteTr
@@ -44,6 +52,22 @@ function montaTd(data, classe) {
     return td;
 }
 
-function validaPaciente(paciente){
-  
+function validaPaciente(paciente) {
+    var erros = [];
+    if (!validaPeso(paciente.peso))
+        erros.push("Peso é inválido");
+    if (!validaAltura(paciente.altura))
+        erros.push("Altura é inválido");
+
+    return erros;
+
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    erros.forEach( (erro) => {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li)
+    });
 }
