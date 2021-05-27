52f559d68f473f39faa8fc8b269ba7eadce70684
Maurício Fonseca <mauricio.lacerdaml@gmail.com>
2021-05-26 22:54:55 -0300

    adição de comentários durante a explicação do código prar os alunos

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,27 +1,24 @@
 var botaoBuscar = document.querySelector("#buscar-paciente");
 
-botaoBuscar.addEventListener("click", function () {
+botaoBuscar.addEventListener("click", () => {
     console.log("...bucando");
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
+    var xhr = new XMLHttpRequest();//serve para realizarmos requisições http
+    xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes"); 
+    //acessar uma api através de uma url e um verbo http
     var erro = document.querySelector("#erro-ajax");
-    xhr.addEventListener("load", function () {
-        if(xhr.status == 200){
+    xhr.addEventListener("load", () => {
+        if (xhr.status == 200) {
             var resposta = xhr.responseText;
             var pacientes = JSON.parse(resposta);
             erro.classList.add("#erro-ajax")
-            pacientes.forEach(function (paciente) {
+            pacientes.forEach((paciente) => {
                 adicionaPacienteNaTabela(paciente)
             });
-        }else{
+        } else {
             console.log(xhr.status);
             console.log(xhr.responseText);
             erro.classList.remove("invisivel")
         }
-       
     });
-
-    xhr.send();
-
+    xhr.send(); //realiza o envio da minha api
 });
